1386b3561e778733f3cf3d14ded96ea42503e187
dill doe <69736850+Levon-Volodin@users.noreply.github.com>
2026-07-31 16:09:16 -0500

    Updated loader.h
    *Lowecased "windows.h" for compatibility for MinGW
    
    Signed-off-by: dill doe <69736850+Levon-Volodin@users.noreply.github.com>

--- client/inject/loader.h
+++ client/inject/loader.h
@@ -8,7 +8,7 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
-#include <Windows.h>
+#include <windows.h>
 
 /*
  * RflData — passed as the thread argument to the reflective loader.
